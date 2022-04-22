1bfb2d685e01ac1506b3b2f9e66268de3b57ee81
Jessica Janei <webdevjessij@gmail.com>
2022-04-21 21:47:06 -0400

    render cards and add styling

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./MemoryCard.css";
+
+class MemoryCard extends React.Component {
+    render() {
+        return (
+            <div className='MemoryCard'>
+                <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="digital crafts logo"/>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default MemoryCard;
